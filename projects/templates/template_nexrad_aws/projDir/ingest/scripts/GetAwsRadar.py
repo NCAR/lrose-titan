040157104a65557dc2e1f177b9813964b4501013
mike-dixon <dixon@ucar.edu>
2019-04-20 19:00:10 -0600

    GetAwsRadar.py

--- projects/templates/template_nexrad_aws/projDir/ingest/scripts/GetAwsRadar.py
+++ projects/templates/template_nexrad_aws/projDir/ingest/scripts/GetAwsRadar.py
@@ -30,8 +30,11 @@ def main():
     global tmpDir
     global singleTime, startTime, endTime
     global doSingle, doInterval
-    global count
-    count = 0
+    global fileCount
+
+    # initialize file count
+
+    fileCount = 0
 
     global thisScriptName
     thisScriptName = os.path.basename(__file__)
@@ -70,70 +73,47 @@ def main():
 
     if (doSingle):
         # single time
-        getForInterval(options.radarName, singleTime, singleTime)
+        startDay = datetime.date(startTime.year, startTime.month, startTime.day)
+        getForInterval(options.radarName, startDay, singleTime, singleTime)
     else:
         if (startTime.day == endTime.day):
             # single day
-            getForInterval(options.radarName, startTime, endTime)
+            startDay = datetime.date(startTime.year, startTime.month, startTime.day)
+            getForInterval(options.radarName, startDay, startTime, endTime)
         else:
             # multiple days
             tdiff = endTime - startTime
             tdiffSecs = tdiff.total_seconds()
-            print("11111111 diff in secs:  ", tdiffSecs, file=sys.stderr)
+            if (options.debug):
+                print("Proc interval in secs:  ", tdiffSecs, file=sys.stderr)
             startDay = datetime.date(startTime.year, startTime.month, startTime.day)
             endDay = datetime.date(endTime.year, endTime.month, endTime.day)
             thisDay = startDay
             while (thisDay <= endDay):
-                print("11111111 processing day:  ", thisDay, file=sys.stderr)
+                if (options.debug):
+                    print("===>>> processing day:  ", thisDay, file=sys.stderr)
                 if (thisDay == startDay):
                     # get to end of start day
                     periodStart = startTime
                     periodEnd = datetime.datetime(thisDay.year, thisDay.month, thisDay.day,
                                                   23, 59, 59)
-                    getForInterval(options.radarName, periodStart, periodEnd)
+                    getForInterval(options.radarName, thisDay, periodStart, periodEnd)
                 elif (thisDay == endDay):
                     # get from start of end day
                     periodStart = datetime.datetime(thisDay.year, thisDay.month, thisDay.day,
                                                     0, 0, 0)
                     periodEnd = endTime
-                    getForInterval(options.radarName, periodStart, periodEnd)
+                    getForInterval(options.radarName, thisDay, periodStart, periodEnd)
                 else:
                     # get for the full day
                     periodStart = datetime.datetime(thisDay.year, thisDay.month, thisDay.day,
                                                     0, 0, 0)
                     periodEnd = datetime.datetime(thisDay.year, thisDay.month, thisDay.day,
                                                   23, 59, 59)
-                    getForInterval(options.radarName, periodStart, periodEnd)
+                    getForInterval(options.radarName, thisDay, periodStart, periodEnd)
                 thisDay = thisDay + datetime.timedelta(1)
                 
-    # get current date and time
-
-    # nowTime = time.gmtime()
-    # now = datetime.datetime(nowTime.tm_year, nowTime.tm_mon, nowTime.tm_mday,
-    #                         nowTime.tm_hour, nowTime.tm_min, nowTime.tm_sec)
-    # nowDateStr = now.strftime("%Y%m%d")
-    # nowDateTimeStr = now.strftime("%Y%m%d%H%M%S")
-
-    # compute time strings
-
-    # startDateTimeStr = startTime.strftime("%Y%m%d%H%M%S")
-    # startDateStr = startTime.strftime("%Y%m%d")
-    # endDateTimeStr = endTime.strftime("%Y%m%d%H%M%S")
-    # endDateStr = endTime.strftime("%Y%m%d")
-
-    # set up list of days to be checked
-
-    # timeInterval = endTime - startTime
-    # if (options.debug):
-    #     print("  startTime: ", startTime, file=sys.stderr)
-    #     print("  endTime: ", endTime, file=sys.stderr)
-    #     print("  timeInterval: ", timeInterval, file=sys.stderr)
-    #     print("  nDays: ", timeInterval.days, file=sys.stderr)
-
-    if (count == 0):
-        print("---->> No files to download", file=sys.stderr)
-    else:
-        print("---->> Downloaded files, n = ", count, file=sys.stderr)
+    print("---->> Num files downloaded: ", fileCount, file=sys.stderr)
         
     endString = "END: " + thisScriptName
     nowTime = datetime.datetime.now()
@@ -148,13 +128,17 @@ def main():
 ########################################################################
 # Get the data for the specified time interval
 
-def getForInterval(radarName, startTime, endTime):
+def getForInterval(radarName, thisDay, startTime, endTime):
+
+    # get the local file list, i.e. files already downloaded
+
+    localFileList = getLocalFileList(thisDay, radarName)
 
     # construct the URL
 
-    dateStr =  startTime.strftime("%Y/%m/%d")
+    awsDateStr =  startTime.strftime("%Y/%m/%d")
     bucketURL = "http://noaa-nexrad-level2.s3.amazonaws.com"
-    dirListURL = bucketURL+ "/?prefix=" + dateStr + "/" + radarName
+    dirListURL = bucketURL+ "/?prefix=" + awsDateStr + "/" + radarName
     if (options.debug):
         print("dirListURL: ", dirListURL, file=sys.stderr)
 
@@ -165,67 +149,42 @@ def getForInterval(radarName, startTime, endTime):
     if (options.debug):
         print("number of keys found: ", len(itemlist), file=sys.stderr)
 
-    process = 0
-    lastFileTime = datetime.datetime(1970, 1, 1, 0, 0, 0);
-    lastFile = ""
     for x in itemlist:
-        file = getText(x.childNodes)
         # Only process files that look like "2012/07/02/KJAX/KJAX20120702_030836_V06.gz"
+        fileEntry = str(getNodeText(x.childNodes))
         try:
-            year = int(file[20:24]);
-            month = int(file[24:26]);
-            day = int(file[26:28]);
-            hour = int(file[29:31]);
-            min = int(file[31:33]);
-            sec = int(file[33:35]);
-            fileTime = datetime.datetime(year, month, day, hour, min, sec);
+            (fyear, fmonth, fday, rname, fileName) = str.split(fileEntry, '/')
+            if (options.verbose):
+                print("==>> fyear, fmonth, fday, rname, fname: ",
+                      fyear, fmonth, fday, rname, fileName, file=sys.stderr)
+            year = int(fileName[4:8]);
+            month = int(fileName[8:10]);
+            day = int(fileName[10:12]);
+            hour = int(fileName[13:15]);
+            minute = int(fileName[15:17]);
+            sec = int(fileName[17:19]);
+            fileTime = datetime.datetime(year, month, day, hour, minute, sec);
         except:
             if(options.verbose):
-                print("ignoring entry, file: ", file, file=sys.stderr)
+                print("bad entry: ", fileEntry, file=sys.stderr)
             continue
 
         if(options.verbose):
-            print("file, time: ", file, fileTime, file=sys.stderr)
+            print("checking file, time: ", fileEntry, fileTime, file=sys.stderr)
 
-        if(process == 0 and startTime < fileTime and endTime > lastFileTime):
-            process = 1
-            download(lastFile,radarName)
-        if(endTime < fileTime):
-            process = 0
-
-        if(process):
-            download(file,radarName)
-            
-        lastFileTime = fileTime
-        lastFile = file
-         
-    
-########################################################################
-# Download specified file
-
-def download(filepath, radarName):
-
-    global count
-
-    dataURL = "https://noaa-nexrad-level2.s3.amazonaws.com"
-    print("Downloading: ", filepath, file=sys.stderr)
-
-    if(options.dryRun == False):
-        (dir,file) = os.path.split(filepath)
-        local = open(os.path.join(options.outputDir,file),'wb')
-        try:
-            opener = urllib.URLopener()
-            myfile = opener.open(os.path.join(dataURL,filepath))
-            local.write(myfile.read())
-            local.close()
-            count = count + 1
-        except OSError as e:
-            print("ERROR: Got a URL error: ", e, file=sys.stderr)
+        if(fileTime >= startTime and fileTime <= endTime):
+            if (options.force):
+                doDownload(radarName, fileTime, fileEntry, fileName)
+            else:
+                if (fileName not in localFileList):
+                    doDownload(radarName, fileTime, fileEntry, fileName)
+                elif (options.debug):
+                    print("file previously downloaded: ", fileName, file=sys.stderr)
     
 ########################################################################
 # Get text for nodes
 
-def getText(nodelist):
+def getNodeText(nodelist):
     rc = []
     for node in nodelist:
         if node.nodeType == node.TEXT_NODE:
@@ -234,36 +193,88 @@ def getText(nodelist):
 
 
 ########################################################################
-# Download a file into the current directory
+# Download specified file
 
-def downloadFile(ftp, dateStr, timeStr, fileName):
-    
+def doDownload(radarName, fileTime, fileEntry, fileName):
+
+    global fileCount
+
     if (options.debug):
-        print("  downloading file: ", fileName, file=sys.stderr)
-        
-    # get file, store in tmp
+        print("Downloading entry, name: ",
+              fileEntry, ", ", fileName, file=sys.stderr)
 
+    if(options.dryRun):
+        # no download
+        return
+
+    # download into tmp file
+
+    dataURL = "https://noaa-nexrad-level2.s3.amazonaws.com"
     tmpPath = os.path.join(options.tmpDir, fileName)
+    try:
+        tmpFile = open(tmpPath, 'wb')
+        opener = urllib.URLopener()
+        myfile = opener.open(os.path.join(dataURL,fileEntry))
+        tmpFile.write(myfile.read())
+        tmpFile.close()
+        fileCount = fileCount + 1
+    except OSError as e:
+        print("ERROR: Got error: ", e, file=sys.stderr)
 
-    if (options.verbose):
-        print("retrieving file, storing as tmpPath: ", tmpPath, file=sys.stderr)
-    ftp.retrbinary('RETR '+ fileName, open(tmpPath, 'wb').write)
+    # create final dir
 
-    # move to final location - i.e. this directory
+    dateStr =  fileTime.strftime("%Y%m%d")
+    outDayDir = os.path.join(options.outputDir, dateStr)
+    try:
+        os.makedirs(outDayDir)
+    except OSError as exc:
+        if (options.verbose):
+            print("WARNING: trying to create dir: ", outDayDir, file=sys.stderr)
+            print("Exception: ", exc, file=sys.stderr)
     
-    cmd = "mv " + tmpPath + " ."
+    # move to output dir
+    
+    cmd = "mv " + tmpPath + " " + outDayDir
     runCommand(cmd)
 
     # write latest_data_info
     
+    timeStr = fileTime.strftime("%Y%m%d%H%M%S")
     relPath = os.path.join(dateStr, fileName)
-    cmd = "LdataWriter -dir " + options.targetDir \
+    cmd = "LdataWriter -dir " + options.outputDir \
           + " -rpath " + relPath \
           + " -ltime " + timeStr \
           + " -writer " + thisScriptName \
           + " -dtype bufr"
     runCommand(cmd)
 
+########################################################################
+# Get list of files already downloaded
+
+def getLocalFileList(date, radarName):
+
+    # make the target directory and go there
+    
+    dateStr = date.strftime("%Y%m%d")
+    localDayDir = os.path.join(options.outputDir, dateStr)
+    try:
+        os.makedirs(localDayDir)
+    except OSError as exc:
+        if (options.verbose):
+            print("WARNING: trying to create dir: ", localDayDir, file = sys.stderr)
+            print("  exception: ", exc, file = sys.stderr)
+
+    # get local file list - i.e. those which have already been downloaded
+    
+    os.chdir(localDayDir)
+    localFileList = os.listdir(localDayDir)
+    localFileList.reverse()
+
+    if (options.verbose):
+        print("==>> localFileList: ", localFileList, file=sys.stderr)
+
+    return localFileList
+            
 ########################################################################
 # Parse the command line
 
@@ -389,7 +400,7 @@ def parseArgs():
 def runCommand(cmd):
 
     if (options.debug):
-        print >>sys.stderr, "running cmd:",cmd
+        print("running cmd: ", cmd, file=sys.stderr)
     
     try:
         retcode = subprocess.call(cmd, shell=True)
